c01df4bbe439d69bb61d16c2aa896168caed67be
Thomas GUEDON <thomas.guedon2022@campus-eni.fr>
2023-05-15 16:21:16 +0200

    creation BO

---
+++ EnCher/src/fr/eni/EnCher/bo/Enchere.java
@@ -0,0 +1,11 @@
+package fr.eni.EnCher.bo;
+
+import java.time.LocalDateTime;
+
+public class Enchere {
+	private int idEnchere;
+	private LocalDateTime dateHeureEnchere;
+	private int montant;
+	private Utilisateur encheriseur;
+	private Article article;
+}
