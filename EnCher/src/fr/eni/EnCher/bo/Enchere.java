5a9dfebfc6f8060e97819b769227e008febf5944
Thomas GUEDON <thomas.guedon2022@campus-eni.fr>
2023-05-17 17:00:57 +0200

    update .gitignore and remane function

--- EnCher/src/fr/eni/EnCher/bo/Enchere.java
+++ EnCher/src/fr/eni/EnCher/bo/Enchere.java
@@ -6,7 +6,7 @@ public class Enchere {
 	private int idEnchere;
 	private LocalDateTime dateHeureEnchere;
 	private int montant;
-	private Utilisateur encheriseur;
+	private Utilisateur encherisseur;
 	private Article article;
 	
 	public Enchere(int idEnchere, LocalDateTime dateHeureEnchere, int montant, Utilisateur encheriseur,
@@ -15,7 +15,7 @@ public class Enchere {
 		this.idEnchere = idEnchere;
 		this.dateHeureEnchere = dateHeureEnchere;
 		this.montant = montant;
-		this.encheriseur = encheriseur;
+		this.encherisseur = encheriseur;
 		this.article = article;
 	}
 
@@ -35,8 +35,8 @@ public class Enchere {
 		return montant;
 	}
 
-	public Utilisateur getEncheriseur() {
-		return encheriseur;
+	public Utilisateur getEncherisseur() {
+		return encherisseur;
 	}
 
 	public Article getArticle() {
