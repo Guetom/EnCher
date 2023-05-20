3249840c253e526475c57c41abfb2504f95461f5
Guetom <thomascacatoes@gmail.com>
2023-05-20 19:01:12 +0200

    add session systeme

---
+++ EnCher/src/fr/eni/EnCher/servlet/Authentification.java
@@ -0,0 +1,62 @@
+package fr.eni.EnCher.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class Authentification
+ */
+@WebFilter("/Authentification")
+public class Authentification implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Authentification() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+        
+        //Check si il y a une session
+        if (session == null) {
+        	//Il y a pas de session
+            chain.doFilter(request, response);
+        } else {
+            //Il y a une session
+            res.sendRedirect(req.getContextPath() + "/");
+        }
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
