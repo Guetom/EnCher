73e5908f7a3f6abd0cef6ca98033f423957615a6
AMIRALADAMS <j.brouillard430@gmail.com>
2023-05-24 16:32:47 +0200

    mise à jour bdd

--- EnCher/WebContent/DLL_EnCher_insert.sql
+++ EnCher/WebContent/DLL_EnCher_insert.sql
@@ -25,7 +25,7 @@ INSERT INTO TAGS (libelle) VALUES ('art');
     --libelle
 INSERT INTO CATEGORIES (libelle) VALUES ('Informatique');
 INSERT INTO CATEGORIES (libelle) VALUES ('Ameublement');
-INSERT INTO CATEGORIES (libelle) VALUES ('Vétement');
+INSERT INTO CATEGORIES (libelle) VALUES ('Vêtement');
 INSERT INTO CATEGORIES (libelle) VALUES ('Sport, Loisirs');
 -- En plus
 INSERT INTO CATEGORIES (libelle) VALUES ('Animalerie');
